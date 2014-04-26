7b5b7fbf36ae726f6c7708d41e7f67ee1d65f8b0
momentspace <service@starferry.net>
2014-04-26 14:16:20 +0900

    fizz buzz

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,12 +1,28 @@
-1.upto(100) do |x|
-	if x % 5 == 0 && x % 3 == 0
-		puts "fizzbuzz"
-	elsif x % 3 == 0
-		puts "fizz"
-	elsif x % 5 == 0
-		puts "buzz"
-	else
-		puts x
+class FizzBuzz
+	def fizzbuzz(x)
+		x % 3 == 0 && x % 5 == 0
+	end
+
+	def fizz(x)
+		x % 3 == 0
+	end
+
+	def buzz(x)
+		x % 5 == 0
+	end
+
+	def main
+		1.upto(100) do |x|
+			if fizzbuzz(x)
+			       	puts "fizzbuzz"
+			elsif fizz(x)
+			       	puts "fizz"
+			elsif buzz(x)
+			       	puts "buzz"
+			else
+				puts x
+			end
+		end
 	end
 end
 
